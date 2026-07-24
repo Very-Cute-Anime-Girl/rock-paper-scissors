7fc7f2a55be6e21ec7f99b22bbd0426f9995e8fb
Very-Cute-Anime-Girl <107881952+Very-Cute-Anime-Girl@users.noreply.github.com>
2026-07-24 16:45:19 -0500

    Bucles for, while, do...while

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function playGame() {
     console.log(`Puntaje PC: ${puntajePC}`);
   }
 
-  for (let i = 1; i <= 5; i++) {
+  for (let i = 1; i <= 1; i++) {
     let humanSelection = getHumanChoice();
     let computerSelection = getComputerChoice();
 
@@ -118,3 +118,79 @@ function playGame() {
 }
 
 playGame();
+
+
+// Bucle for...of
+const canidos = ["Lobo", "Perro", "Hiena", "Coyote"];
+
+for (const can of canidos) {
+    console.log(can)
+}
+
+// map() aplica cambios a caada elemento de una colección y lo guarda una nueva
+function toUpper(string) {
+    return string.toUpperCase();
+}
+
+const mayusCanidos = canidos.map(toUpper);
+console.log(mayusCanidos);
+
+// con filter() se dan parámetros para crear una colección nueva con items que lo cumplen
+function lCan(can) {
+    return can.startsWith("P");
+}
+
+const canidosFiltrado = canidos.filter(lCan);
+console.log(canidosFiltrado)
+
+// Búsqueda de contactos
+const contactos = [
+    "Juan:123456789",
+    "Martin:234567891",
+    "Pepe:345678912",
+    "Abdul:456789123",
+    "Said:567891234",
+];
+
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    const searchName = input.value.toLowerCase();
+    input.value = "";
+    input.focus();
+    para.textContent = "";
+    for (const contacto of contactos) {
+        const splitContact = contacto.split(":");
+        if (splitContact[0].toLowerCase() === searchName) {
+            para.textContent = `El número de ${splitContact[0]} es ${splitContact[1]}`;
+            break; // Sale del bucle, deja de buscar.
+        }
+    }
+    if (para.textContent === "") {
+        para.textContent = "No se encontró el contacto.";
+    }
+});
+
+// bucle while y do...while
+let indice = 1
+while (indice<=5) {
+    console.log(indice);
+    indice++;
+}
+
+do {
+    console.log(`mensaje ${indice}`)
+    indice++;
+} while (indice<=10)
+
+// do...while siempre corre al menos una vez, luego checa la condición
+// array -> colección ordenada de items
+
+const arr = [1,2,3,4,5];
+const productoDeTodo = arr.reduce((total, itemActual) => {
+    return total * itemActual;
+}, 1);
+console.log(productoDeTodo)
+console.log(arr)
